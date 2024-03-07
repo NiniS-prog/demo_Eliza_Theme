73f66c03ca0d09762376b29de8390f0a02ae8f56
joni <SegaLearn@gmail.com>
2024-03-07 15:17:50 +0200

    PRODUCT MEDIA slide

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,22 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
+  content: [
+    './layout/*.liquid',
+    './sections/*.liquid',
+    './snippets/*.liquid',
+    './templates/customers/*.liquid',
+    './templates/*.liquid'
+
+
+  ],
   theme: {
     extend: {
       height: {
         '94': '22rem'
       }
     },
-  },
-  variants: {
-    extend: {},
+
   },
   plugins: [],
 }
+
